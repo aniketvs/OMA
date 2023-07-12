d60d30f8e89fbf7fcf974e765199902559675038
aniketvs <sharmavinod8454@gmail.com>
2023-07-11 23:02:24 -0700

    import

--- oma/src/common/Meet/Meet.js
+++ oma/src/common/Meet/Meet.js
@@ -6,21 +6,37 @@ export default function Meet() {
       {/* main div */}
       <div>
         {/*Header div*/}
-        <div>
-              <h1>Meet The Doctors</h1>
-              <p></p>
+        <div className='header-div'>
+          <h1 className='heading'>Meet The Doctors</h1>
+          <p className='p-1'>With a highly qualified team of doctors and trained support staff, we aim to</p>
         </div>
         {/*Doctor Main Div*/}
-        <div>
+        <div className='d-flex flex-row justify-content-center '>
           {/*Doctor div*/}
           <div>
+            <div>
+               <div></div>
+            </div>
+            <h4>Dr. Pankaj Nair</h4>
+            <p>MBBS,DGO,Senior Consoltant</p>
+            <p> Obestric & Gyancologist</p>
+          </div>
+          <div>
+            <div>
+               <div></div>
+            </div>
+            <h4>Dr. Pankaj Nair</h4>
+            <p>MBBS,DGO,Senior Consoltant</p>
+            <p> Obestric & Gyancologist</p>
 
           </div>
           <div>
-
-          </div>
-          <div>
-
+            <div>
+               <div></div>
+            </div>
+            <h4>Dr. Pankaj Nair</h4>
+            <p>MBBS,DGO,Senior Consoltant</p>
+            <p> Obestric & Gyancologist</p>
           </div>
         </div>
 
