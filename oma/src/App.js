f92f0cbd56431b6f3b7ebf395bacab08fcbc5033
aniketvs <sharmavinod8454@gmail.com>
2023-07-11 22:43:10 -0700

    common

--- oma/src/App.js
+++ oma/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Meet from './common/Meet';
 
 function App() {
   return (
    <>
-  
+    <Meet/>
    </>
   );
 }
---
+++ oma/src/common/Meet.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Meet() {
+  return (
+    <>  
+    {/* main div */}
+       <div>
+           {/*Header div*/}
+           <div>
+
+           </div>
+            {/*Doctor Main Div*/}
+
+       </div>
+
+    </>
+  )
+}
