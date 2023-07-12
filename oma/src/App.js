c740fcd1166bad9701d0fc677c0123f46bbe4a2a
1PrathameshMore <theprathamesh01@gmail.com>
2023-07-12 16:30:06 +0530

    Circular component

--- oma/src/App.js
+++ oma/src/App.js
@@ -6,6 +6,7 @@ import Footer from "./common/Footer/Footer"
 import Meet from './common/Meet/Meet';
 import Hero from './common/Hero/Hero';
 import Header from './common/Header/Header';
+import CircularComponent from "./common/CircularComponent/CircularComponent";
 
 function App() {
   return (
@@ -18,6 +19,12 @@ function App() {
         descOne={'Will talk about healthcare center dedicated to woman and childcare, the complexities of women\'s health. Also talk about our team at OMA Hospital ensuring the utmost Priority on Care and Comfort when it comes to Maternity Services.'}
         desctwo={'Our Team is Excellently Trained, Highly Competent and Reputed in Obstetrics. With dozens of Luxuries and Amenities we feel more like a Hotel than a Hospital.'}
       />
+      <CircularComponent />
+      <div className="banner-image">
+        <img 
+          src="/images/CoverImg.png"
+        />
+      </div>
       <Meet />
       <Footer />
     </>
---
+++ oma/src/common/CircularComponent/CircularComponent.js
@@ -0,0 +1,48 @@
+import './CircularComponent.scss'
+
+const CircularComponent = () => {
+  return (
+    <div className='circular'>
+      <div className='circular__wrapper'>
+        <h3>What Makes Oma the best delivery & maternity hospital in mumbai</h3>
+        <div className='circular__image-wrapper'>
+          <img src='/images/maternity-circle.png' alt='maternity circular image' />
+          <div className='text-wrapper t1' >
+            <p>bleeding between periods</p>
+            <div className='dot' />
+          </div>
+          <div className='text-wrapper t2' >
+            <p>frequent and urgent urge to urinate, or <br />a burning sensation during urination</p>
+            <div className='dot' />
+          </div>
+          <div className='text-wrapper t3' >
+            <p>frequent and urgent urge to urinate, or <br />a burning sensation during urination</p>
+            <div className='dot' />
+          </div>
+          <div className='text-wrapper t4' >
+            <p>frequent and urgent urge to urinate, or <br />a burning sensation during urination</p>
+            <div className='dot' />
+          </div>
+          <div className='text-wrapper t5' >
+            <div className='dot' />
+            <p>frequent and urgent urge to urinate, or <br />a burning sensation during urination</p>
+          </div>
+          <div className='text-wrapper t6' >
+            <div className='dot' />
+            <p>frequent and urgent urge to urinate, or <br />a burning sensation during urination</p>
+          </div>
+          <div className='text-wrapper t7' >
+            <div className='dot' />
+            <p>frequent and urgent urge to urinate, or <br />a burning sensation during urination</p>
+          </div>
+          <div className='text-wrapper t8' >
+            <div className='dot' />
+            <p>frequent and urgent urge to urinate, or <br />a burning sensation during urination</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default CircularComponent
