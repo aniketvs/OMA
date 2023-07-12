33978808462a23eedb92457e03a8b0bd96337597
1PrathameshMore <theprathamesh01@gmail.com>
2023-07-12 13:31:49 +0530

    hero

--- oma/src/App.js
+++ oma/src/App.js
@@ -1,15 +1,26 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import logo from './logo.svg';
 import './App.css';
-import Meet from './common//Meet/Meet';
+
 import Footer from "./common/Footer/Footer"
+import Meet from './common/Meet/Meet';
+import Hero from './common/Hero/Hero';
+import Header from './common/Header/Header';
 
 function App() {
   return (
-   <>
-    <Meet/>
-    <Footer/>
-   </>
+    <>
+      <Header />
+      <Hero
+        imgSrc={'/images/Aboutmaternity.png'}
+        header={'Maternity'}
+        headLine={'Keeping Minds at Ease, Through Love, Compassion, and Kindness'}
+        descOne={'Will talk about healthcare center dedicated to woman and childcare, the complexities of women\'s health. Also talk about our team at OMA Hospital ensuring the utmost Priority on Care and Comfort when it comes to Maternity Services.'}
+        desctwo={'Our Team is Excellently Trained, Highly Competent and Reputed in Obstetrics. With dozens of Luxuries and Amenities we feel more like a Hotel than a Hospital.'}
+      />
+      <Meet />
+      <Footer />
+    </>
   );
 }
 
--- oma/src/common/Hero/Hero.js
+++ oma/src/common/Hero/Hero.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import './Hero.scss'
 
-const Hero = ({ imgSrc, header, headLine, descOne, desctwo, height }) => {
+const Hero = ({ imgSrc, header, headLine, descOne, desctwo, fullHeight }) => {
   return (
     <main className='hero'>
         <div className='hero__content'>
-
+            <h2 className='hero__header'>
+                {header}
+            </h2>
+            <h1 className='hero__headline'>
+                {headLine}
+            </h1>
+            <p className='hero__description'>{descOne}</p>
+            <p className='hero__description'>{desctwo}</p>
         </div>
         <div className='hero__image'>
             <img 
