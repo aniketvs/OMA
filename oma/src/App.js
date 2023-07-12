bdc16b61950c6fc70e5421cebc8a9062b95cab08
aakash0230 <pandeyaakash123.ap@gmail.com>
2023-07-12 13:21:37 +0530

    Footer added

--- oma/src/App.js
+++ oma/src/App.js
@@ -1,16 +1,14 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css"
+import logo from './logo.svg';
 import './App.css';
-import Meet from './common/Meet/Meet';
-import Hero from './common/Hero/Hero';
-import Header from './common/Header/Header';
-import Footer from './common/Footer/Footer'
+import Meet from './common//Meet/Meet';
+import Footer from "./common/Footer/Footer"
+
 function App() {
   return (
    <>
-   <Header/>
-   <Hero/>
-  <Meet/>
-  <Footer/>
+    <Meet/>
+    <Footer/>
    </>
   );
 }
