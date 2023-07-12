e1bbe2422ad822001300c966d3c3d5ba74839119
aakash0230 <pandeyaakash123.ap@gmail.com>
2023-07-12 16:43:15 +0530

    Aesthetic component made

---
+++ oma/src/common/AestheticGynacelogy/AestheticGynacelogy.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import "./aestheticGynacelogy.css"
+
+
+export default function AestheticGynacelogy() {
+  return (
+    <div className='aesthetic-gynaecology-info'>
+        <div className='container'>
+            <div className='upper-content'>
+                <h3 className='heading'>What is Aesthetic Gynaecology</h3>
+                <p> when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                <p><b>Some of the most common procedures associated with cosmetic gynecology include :</b></p>
+            </div>
+            <div className='lower-content'>
+                <div className='row'>
+                    <div className='col-6'>
+                        <div className='d-flex flex-row'>
+                                <div className='aesthetic-icon-div'>
+                                    <div className='aesthetic-icon'></div>
+                                </div>
+                                <div className='side-component'>
+                                    <h5>G - Spot Augmentation</h5>
+                                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                                </div>
+                        </div>
+                    </div>
+                    <div className='col-6'>
+                        <div className='d-flex flex-row'>
+                                <div className='aesthetic-icon-div'>
+                                    <div className='aesthetic-icon'></div>
+                                </div>
+                                <div className='side-component'>
+                                    <h5>G - Spot Augmentation</h5>
+                                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-6'>
+                        <div className='d-flex flex-row'>
+                                <div className='aesthetic-icon-div'>
+                                    <div className='aesthetic-icon'></div>
+                                </div>
+                                <div className='side-component'>
+                                    <h5>G - Spot Augmentation</h5>
+                                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                                </div>
+                        </div>
+                    </div>
+                    <div className='col-6'>
+                        <div className='d-flex flex-row'>
+                                <div className='aesthetic-icon-div'>
+                                    <div className='aesthetic-icon'></div>
+                                </div>
+                                <div className='side-component'>
+                                    <h5>G - Spot Augmentation</h5>
+                                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-6'>
+                        <div className='d-flex flex-row'>
+                                <div className='aesthetic-icon-div'>
+                                    <div className='aesthetic-icon'></div>
+                                </div>
+                                <div className='side-component'>
+                                    <h5>G - Spot Augmentation</h5>
+                                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                                </div>
+                        </div>
+                    </div>
+                    <div className='col-6'>
+                        <div className='d-flex flex-row'>
+                                <div className='aesthetic-icon-div'>
+                                    <div className='aesthetic-icon'></div>
+                                </div>
+                                <div className='side-component'>
+                                    <h5>G - Spot Augmentation</h5>
+                                    <p>when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                                </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
+    </div>
+  )
+}
