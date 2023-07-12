cd80d1c60058e1895e7cd60b4e08c6246b27ab33
bhushan555 <bhushanmayekar5@gmail.com>
2023-07-12 16:37:27 +0530

    Services Added

--- oma/src/App.js
+++ oma/src/App.js
@@ -6,11 +6,7 @@ import Footer from "./common/Footer/Footer"
 import Meet from './common/Meet/Meet';
 import Hero from './common/Hero/Hero';
 import Header from './common/Header/Header';
-<<<<<<< Updated upstream
 import CircularComponent from "./common/CircularComponent/CircularComponent";
-=======
-import Services from "./common/services/Services";
->>>>>>> Stashed changes
 
 function App() {
   return (
@@ -23,16 +19,12 @@ function App() {
         descOne={'Will talk about healthcare center dedicated to woman and childcare, the complexities of women\'s health. Also talk about our team at OMA Hospital ensuring the utmost Priority on Care and Comfort when it comes to Maternity Services.'}
         desctwo={'Our Team is Excellently Trained, Highly Competent and Reputed in Obstetrics. With dozens of Luxuries and Amenities we feel more like a Hotel than a Hospital.'}
       />
-<<<<<<< Updated upstream
       <CircularComponent />
       <div className="banner-image">
         <img 
           src="/images/CoverImg.png"
         />
       </div>
-=======
-      <Services/>
->>>>>>> Stashed changes
       <Meet />
       <Footer />
     </>
